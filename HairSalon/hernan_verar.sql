d1cc95bcf8dc9779c94262a0de10b1f4b1abf7be
Hernan Verar <66854899+hernanverar@users.noreply.github.com>
2023-07-30 21:50:41 -0700

    Update hernan_verar.sql

--- HairSalon/hernan_verar.sql
+++ HairSalon/hernan_verar.sql
@@ -1,9 +1,8 @@
 -- MySQL dump 10.13  Distrib 8.0.27, for macos11 (x86_64)
 --
--- Host: localhost    Database: hernan_verar
-
+-- Host: 127.0.0.1    Database: hernan_verar
 -- ------------------------------------------------------
--- Server version	8.0.27
+-- Server version	8.0.33
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -17,34 +16,20 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `Clients`
+-- Table structure for table `stylists`
 --
 
-DROP TABLE IF EXISTS `Clients`;
+DROP TABLE IF EXISTS `stylists`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Clients` (
-  `ClientId` int NOT NULL AUTO_INCREMENT,
-  `ClientName` varchar(255) DEFAULT NULL,
-  `StylistId` int NOT NULL,
-  PRIMARY KEY (`ClientId`),
-  KEY `StylistId_idx` (`StylistId`),
-  CONSTRAINT `StylistId` FOREIGN KEY (`StylistId`) REFERENCES `Stylists` (`StylistId`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `Stylists`
---
-
-DROP TABLE IF EXISTS `Stylists`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Stylists` (
-  `StylistId` int NOT NULL AUTO_INCREMENT,
-  `StylistName` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`StylistId`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `stylists` (
+  `stylistsid` int NOT NULL AUTO_INCREMENT,
+  `stylistsname` varchar(255) DEFAULT NULL,
+  `clientid` int DEFAULT NULL,
+  `clientname` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`stylistsid`),
+  UNIQUE KEY `stylistsid_UNIQUE` (`stylistsid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -56,4 +41,4 @@ CREATE TABLE `Stylists` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-03-10 14:39:46
+-- Dump completed on 2023-07-28 16:33:02
