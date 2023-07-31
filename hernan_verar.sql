fc74441bdcf0c7511939277b5bb793ded9fbbf84
Hernan Verar <66854899+hernanverar@users.noreply.github.com>
2023-07-30 19:14:23 -0700

    Update hernan_verar.sql

--- hernan_verar.sql
+++ hernan_verar.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.27, for macos11 (x86_64)
 --
--- Host: localhost    Database: brent_hickman
+-- Host: localhost    Database: hernan_verar
 -- ------------------------------------------------------
 -- Server version	8.0.27
 
